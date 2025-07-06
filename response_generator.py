7b462510fd44c8df279725c64c10540637f3ba34
Yashasvaddi <yashas.vaddi12@gmail.com>
2025-07-06 18:25:50 +0530

    Almost work done

---
+++ response_generator.py
@@ -0,0 +1,62 @@
+import os
+import google.generativeai as genai
+import numpy as np
+import pandas as pd
+import faiss
+
+genai.configure(api_key="AIzaSyC3vNkSnEJl-eFloSm9M4Bw0F_cJv2vusY")
+model=genai.GenerativeModel('gemini-1.5-flash')
+
+working_dir=os.path.dirname(os.path.abspath(__file__))
+database_path=os.path.join(working_dir,'database','embeddings.csv')
+embedding_path=os.path.join(working_dir,'vectorstore','vectorembedding.npy')
+faiss_index_path=os.path.join(working_dir,'vectorstore','faiss.index')
+
+
+embedding_vector=np.load(embedding_path)
+df=pd.read_csv(database_path)
+index=faiss.read_index(faiss_index_path)
+
+def get_embedding(value):
+    response=genai.embed_content(
+        model='models/embedding-001',
+        content=value,
+        task_type="retrieval_query"
+    )
+    vector=np.array(response['embedding'],dtype=np.float32)
+    return vector/np.linalg.norm(vector)
+
+
+def response_gen(query,top_k):
+    query_embedding=get_embedding(query).reshape(1,-1)
+    distances,indices=index.search(query_embedding,top_k)
+    top_index=indices[0][0]
+    top_row=df.iloc[top_index]
+    similarity=distances[0][0]*100
+    context=f"You are a chatbot called 'ASKGDG'and you are here to assist people. Talk as if you are customer support executive and send response to the question {query} in max 2 lines. Dont say anything apart from the answer to the question."
+    example=f'''
+        Question: What is GDG?
+        Answer: GDG is a committee called as GOOGLE DEVELOPERS GROUP.
+        Notes: No need to use 'Here is the answer to' or any such similar sentences.
+    '''
+    remember=f"You are  not allowed to answer anything that is not related to GOOGLE, THADOMAL SHAHANI ENGINEERING COLLEGE or GDG."
+    
+    if similarity<75:
+        Question=f'''{context}
+        {example}
+        {remember}'''
+        response=model.generate_content(Question)
+        print(response.text)
+    else:
+        print(top_row['Answer'],similarity)
+
+
+if __name__=='__main__':
+    ask="How can i help you?"
+    count=0
+    while True:
+        if count>=1:
+            ask="Is there anything else that i can help u with?"
+        query=input(f"{ask}\n")
+        response_gen(query,1)
+        count=count+1
