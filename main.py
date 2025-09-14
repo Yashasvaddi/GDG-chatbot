94f76072f98675b40f79e2d774ded92f3a0883e3
YASHAS VADDI <62982638+Yashasvaddi@users.noreply.github.com>
2025-09-14 17:26:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
+app=FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -15,8 +17,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-app=FastAPI()
 
 load_dotenv()
 
